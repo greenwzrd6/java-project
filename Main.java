69ee366f476d1ee8360b51e00faf6db7546315c5
David Skansholm <valkeshar@gmail.com>
2025-10-21 15:04:01 +0200

    fixing errors again

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello from Person A and B!");
-	System.out.prinln("Hello I am person A HIIHIHIHIH");
+    System.out.prinln("Hello I am person A HIIHIHIHIH");
     System.out.println("Person B wrote this");
+    System.out.println(
+        """
+        Tjenare :DDDDDDDD
+        """); // Person A
+     System.out.println("Person B wrote this");
   }
 }
