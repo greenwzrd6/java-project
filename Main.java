35785770196080feb868aae45d55c7714de948c5
greenwzrd6 <t4kikun@gmail.com>
2025-10-21 15:01:36 +0200

    Added prinln person A

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello from Person A and B!");
-	System.out.println("Person B wrote this");
+	System.out.prinln("Hello I am person A HIIHIHIHIH");
+    System.out.println("Person B wrote this");
   }
 }
